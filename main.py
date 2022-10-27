13f2c199322fb18d7de7aa70fc0e9abaa0c4fc3c
android studio <mobil@NAVIATK.RU>
2022-10-27 09:47:07 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import math
+from multiprocessing import Queue
+import multiprocessing
+from datetime import datetime
+import watchdog
+
+
+def process(insert: Queue, output: Queue):
+    while True:
+        a, b = insert.get(), insert.get()
+        powered = int(math.pow(a, b))
+        summed = 0
+        for i in range(powered + 1):
+            summed += i
+        output.put(f"{a} ^ {b} = {powered}, {summed}")
+
+
+def file_writer(queue: Queue):
+    while True:
+        if not queue.empty():
+            now = datetime.now()
+            file = open(f"watch/{now.day} - {now.month} - {now.year}  {now.hour}-{now.minute}-{now.second}  --  {queue.get()}", "w+")
+            file.close()
+
+
+if __name__ == '__main__':
+    in_queue = Queue()
+    out_queue = Queue()
+    multiprocessing.Process(target=file_writer, args=(out_queue, )).start()
+    multiprocessing.Process(target=process, args=(in_queue, out_queue, )).start()
+    while True:
+        inp = input()
+        in_queue.put(int(inp.split()[0]))
+        in_queue.put(int(inp.split()[1]))
+
+
+
+
+
